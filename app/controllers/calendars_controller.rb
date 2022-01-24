de4715a0bef6080f0c83b3cdabd37d8fff4f0bb7
gaki-h <walter.thi.gks@gmail.com>
2022-01-24 12:32:39 +0900

    Revert "issue1~3の修正_2回目"
    This reverts commit c73a0a67e5f602f737a7d648ac8751d184ac0727.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,7 +29,7 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-
+    
     7.times do |x|
       today_plans = []
       plans.each do |plan|
@@ -40,4 +40,3 @@ class CalendarsController < ApplicationController
     end
   end
 end
-
